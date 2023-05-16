911b12c2f1be3cce822a1e49a968d148e1d4a465
Kaan Zati <kaan.zati@gmail.com>
2023-05-16 15:11:36 +0300

    Final

--- main.py
+++ main.py
@@ -24,6 +24,10 @@ time_leo.goto(0, 300)
 time_leo.write(f"Time: {start_time}", align="center", font=("Courier", 24, "normal"))
 
 
+
+
+
+
 def timer():
     global start_time
 
@@ -39,7 +43,6 @@ def timer():
     leo_over.penup()
     leo_over.goto(0, 0)
     leo_over.write(f"Game Over\nYour Score: {score}", align="center", font=("Courier", 24, "normal"))
-
 def leo_position():
     xy_pos_list = list(range(-200, 200))
     while start_time >= 0:
@@ -62,9 +65,8 @@ score_leo.goto(0, 260)
 score_leo.write(f"Score: {score}", align="center", font=("Courier", 24, "normal"))
 
 turtle.listen()
-turtle.onkey(start_game, "s")  # "s" tuşuna basıldığında start_game fonksiyonunu çağır
-turtle.onkey(exit_game, "e")  # "e" tuşuna basıldığında exit_game fonksiyonunu çağır
-turtle.onkey(reset_game, "r")  # "r" tuşuna basıldığında reset_game fonksiyonunu çağır
+
+
 
 leo.onclick(scoring)
 
